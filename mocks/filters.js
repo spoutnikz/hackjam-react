3ed6504e39f85fdc18c6684c76f4fa68aead161b
davyengone <engonenzedavy@yahoo.com>
2016-05-23 12:01:54 +0200

    added more filters

--- mocks/filters.js
+++ mocks/filters.js
@@ -3,6 +3,7 @@ const filters = [
   { category: 'Web', selected: false},
   { category: 'Mobile', selected: false},
   { category: 'IoT', selected: false},
+  { category: 'Data Engineering', selected: false},
 ];
 
 export default filters;
